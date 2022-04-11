d16ab72607941cebcac9d5d52e6258f2bb763111
Sooyoung <sooyoung159@naver.com>
2022-04-11 21:20:32 +0900

    model

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 const express = require("express");
-
 const dotenv = require("dotenv");
 dotenv.config();
+const bodyParser = require("body-parser");
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.NEXT_PUBLIC_PORT || 4000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
+const { User } = require("./models/User");
 mongoose
   .connect(
-    `mongodb+srv://sooyoung:${process.env.MONGOOSE_PASSWORD}@cluster0.vf18v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    `mongodb+srv://sooyoung:${process.env.NEXT_PUBLIC_MONGOOSE_PASSWORD}@cluster0.vf18v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -19,6 +23,17 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("잘되나?");
+});
+
+app.post("/register", (req, res) => {
+  // 회원 가입할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => {
---
+++ server/models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
+
+userSchema.pre("save", (next) => {
+  var user = this;
+  // 비밀번호를 암호화 시킨다.
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = { User };
